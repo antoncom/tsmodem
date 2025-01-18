b44d24788f699da016bf006f202dbe81b0d4792c
anti <aos@rosze.ru>
2025-01-18 23:36:53 +0300

    19_Правило: добавлено чтение конфига

--- usr/lib/lua/applogic/rule/19_rule.lua
+++ usr/lib/lua/applogic/rule/19_rule.lua
@@ -7,100 +7,74 @@ local rule_setting = {
 	title = {
 		input = "Правило периодического опроса GPIO",
 	},
+
+	IO0_config = {
+		note = "Конфигурация GPIO из /etc/config/tsmgpio",
+		input = "",
+        source = {
+            type = "ubus",
+            object = "uci",
+            method = "get",
+            params = {
+                config = "tsmgpio",
+                section = "IO_0",
+                --option = "direction"
+            },
+        },
+	},
+
 	IO0 = {
 		note = [[ Идентификатор GPIO линия 0 ]],
 		source = {
 			type = "ubus",
 			object = "tsmodem.gpio",
 			method = "IO0",
-			params = {},
+            params = {
+                value = "0",
+                direction = "in",
+                trigger = "none"
+            },
 		},
 		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
+			--["1_bash"] = [[ jsonfilter -e '$.response.value' ]]
 		}
 	},
-	IO1 = {
-		note = [[ Идентификатор GPIO линия 1 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO1",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},	
-	IO2 = {
-		note = [[ Идентификатор GPIO линия 0 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO2",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},	
-	IO3 = {
-		note = [[ Идентификатор GPIO линия 3 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO0",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},	
-	IO4 = {
-		note = [[ Идентификатор GPIO линия 4 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO0",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},
-	IO5 = {
-		note = [[ Идентификатор GPIO линия 5 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO0",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},	
-	IO6 = {
-		note = [[ Идентификатор GPIO линия 6 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO0",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},	
-	IO7 = {
-		note = [[ Идентификатор GPIO линия 7 ]],
-		source = {
-			type = "ubus",
-			object = "tsmodem.gpio",
-			method = "IO0",
-			params = {},
-		},
-		modifier = {
-			["1_bash"] = [[ jsonfilter -e $.value ]]
-		}
-	},				
 }
+
+function rule:make()
+	debug_mode.level = "ERROR"
+	rule.debug_mode = debug_mode
+	local ONLY = rule.debug_mode.level
+
+	-- These variables are included into debug overview (run "applogic debug" to get all rules overview)
+	-- Green, Yellow and Red are measure of importance for Application logic
+	-- Green is for timers and some passive variables,
+	-- Yellow is for that vars which switches logic - affects to normal application behavior
+	-- Red is for some extraordinal application ehavior, like watchdog, etc.
+	local overview = {
+		["do_switch"] = { ["yellow"] = [[ return ($do_switch == "true") ]] },
+		["timeout"] = { ["yellow"] = [[ return (tonumber($timeout) and tonumber($timeout) < 600) ]] },
+		["send_command"] = { ["yellow"] = [[ return ($send_command == "true") ]] },
+		["a_balance_interval"] = { ["green"] = [[ return true ]] },
+	}
+
+	self:load("title"):modify():debug()
+	self:load("IO0_config"):modify():debug()
+	self:load("IO0"):modify():debug()
+
+end
+
+---[[ Initializing. Don't edit the code below ]]---
+local metatable = {
+	__call = function(table, parent)
+		local t = rule_init(table, rule_setting, parent)
+		if not t.is_busy then
+			t.is_busy = true
+			t:make()
+			t.is_busy = false
+		end
+		return t
+	end
+}
+setmetatable(rule, metatable)
+return rule
