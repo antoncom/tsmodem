7846379def5841d4f70df65659112a3282d87631
anti <aos@rosze.ru>
2025-04-13 01:24:32 +0300

    Выполнение Bash скрипта по событию GPIO - Работает.Требует тщательное тестирование на железе.

--- tsmgpio/driver/gpio.lua
+++ tsmgpio/driver/gpio.lua
@@ -12,15 +12,32 @@ function gpio:init()
 end
 
 function gpio:ActionOnEvent()
-	if(gpio.notifier.gpio_change_detected) then
-		-- Обновление данных по конфигурации
-		-- TODO: выполнять чтение только если MD5sum 
-		-- конфига отличается
-		gpio.confgpio:GetGPIOconfig()
-		print("***********GPIO Event detected**********")
-		util.dumptable(gpio.notifier.gpio_scan_result)
-		gpio.notifier.gpio_change_detected = false
-	end
+    if gpio.notifier.gpio_change_detected then
+        -- Обновление данных по конфигурации
+        gpio.confgpio:GetGPIOconfig()
+        print("***********GPIO Event detected**********")
+        -- Проходим по всем изменившимся GPIO
+        for io_name, io_data in pairs(gpio.notifier.gpio_scan_result) do
+            if io_data.direction == "in" then
+                -- Используем имя в формате "IO0", "IO1" (как в конфиге)
+                local section_name = io_name                
+                -- Проверяем, что секция существует и включена
+                local gpio_config = gpio.confgpio.gpio_config_cache[section_name]
+                if gpio_config and gpio_config.status == "enable" then
+                    local action_cmd = gpio_config.action_command
+                    print("Action command for " .. io_name .. ": " .. (action_cmd or "not set"))                  
+                    -- Если команда задана - выполняем
+                    if action_cmd and action_cmd ~= "" then
+                        print("Executing: " .. action_cmd)
+                        os.execute(action_cmd)
+                    end
+                else
+                    print("No config found for " .. section_name .. " or GPIO disabled")
+                end
+            end
+        end
+        gpio.notifier.gpio_change_detected = false
+    end
 end
 
 local metatable = {
